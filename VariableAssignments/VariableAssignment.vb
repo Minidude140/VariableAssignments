58e1e9d9524767fe12fa9f1248f232398903fe32
Zac <zac.minidude140@Gmail.com>
2023-08-24 08:53:56 -0600

    Variable Declaration

--- VariableAssignments/VariableAssignment.vb
+++ VariableAssignments/VariableAssignment.vb
@@ -1,6 +1,12 @@
 ﻿Module VariableAssignment
 
     Sub Main()
+        Dim fruit As String
+        Dim count As Integer
+        Dim price As Double
+        fruit = "Apple"
+        count = 5
+        price = 1.99
 
     End Sub
 
