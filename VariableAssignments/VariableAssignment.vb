9e66d3a15654e1192ac2c4c9e618f485f86be9d1
Zac <zac.minidude140@Gmail.com>
2023-08-28 09:09:08 -0600

    Add User Interaction

--- VariableAssignments/VariableAssignment.vb
+++ VariableAssignments/VariableAssignment.vb
@@ -8,10 +8,32 @@ Module VariableAssignment
         Dim fruit As String
         Dim count As Integer
         Dim price As Double
+        Dim userInput As String
+        Dim userNum As Integer
         fruit = "Apples"
         count = 5
         price = 1.99
         Console.WriteLine("I have " & count & " " & fruit)
+        userInput = Console.ReadLine()
+        Console.WriteLine("You Typed: " & userInput)
+        Console.WriteLine($"You Typed: {userInput}")
+        'Entropolated string ^^ (Formatted Strings FStrings)
+
+        'Pause for user to press enter
+        'Console.WriteLine("Hit Enter to continue")
+        'Console.Read()
+
+        Console.WriteLine("Please enter a number:")
+        userInput = Console.ReadLine()
+        userNum = CInt(userInput)
+
+
+        'sets userNum to the numeric value of userInput
+        Console.WriteLine("You entered: " & userNum)
+        Console.WriteLine($"{userNum} + {count} = {userNum + count}")
+        ' Displays Number and equation
+
+        Console.WriteLine("Please Press Enter to Close Program")
         Console.Read()
 
     End Sub
